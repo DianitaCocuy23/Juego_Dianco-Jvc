8532ed4a5cdee6b26dbfacd04a8597dbb92845c1
Diana Maria Cocuy Ortiz <dianamariacocuy@gmail.com>
2022-12-02 21:50:16 -0500

    segunda modificacion

--- script.js
+++ script.js
@@ -1,106 +1,238 @@
 var contador_preguntas = 1
+const PUNTOS_POSITIVOS = 100
+const PUNTOS_NEGATIVOS = 60
 const resultados = []
 const preguntas = [
     {
         id: 1,
-        encabezado: "¿Cual es la bandera de Santiago de Cali?",
+        encabezado: "¿En qué hemisferio está situado Colombia?",
         opciones: [
             {
                 id: 1,
-                respuesta: "Azul, Amarillo, Rojo",
+                respuesta: "En el hemisferio sur ",
                 correcto: false
             },
             {
                 id: 2,
-                respuesta: "Azul, Rojo, Blanco, Rojo, Verde",
-                correcto: true
-            },
-            {
-                id: 3,
-                respuesta: "Amarillo, Azul, Verde",
+                respuesta: "En el hemisferio norte",
                 correcto: false
             },
             {
+                id: 3,
+                respuesta: "En los dos hemisferios ",
+                correcto: true
+            },
+            {
                 id: 4,
-                respuesta: "Rojo, Verde, Azul, Blanco",
+                respuesta: "En ninguno",
                 correcto: false
             }
         ]
     },
     {
         id: 2,
-        encabezado: "¿Cual es la bandera de colombia?",
+        encabezado: "Colombia posee costa sobre dos océanos, ¿cuáles son?",
         opciones: [
             {
                 id: 1,
-                respuesta: "Azul, Amarillo, Rojo",
-                correcto: true
+                respuesta: "El Atlántico y el Índico ",
+                correcto: false
             },
             {
                 id: 2,
-                respuesta: "Azul, Rojo, Blanco, Rojo, Verde",
+                respuesta: "El Pacífico y el Caribe",
                 correcto: false
             },
             {
                 id: 3,
-                respuesta: "Rojo, Verde, Azul, Blanco",
-                correcto: false
+                respuesta: "El Pacífico y el Atlántico",
+                correcto: true
             },
             {
                 id: 4,
-                respuesta: "Amarillo, Azul, Verde",
+                respuesta: "El Caribe y el Antártico",
                 correcto: false
             }
         ]
     },
     {
         id: 3,
-        encabezado: "¿Qué día se celebra el día de la independencia de Colombia?",
+        encabezado: "¿Cuántas regiones conforman la geografía colombiana?",
         opciones: [
             {
                 id: 1,
-                respuesta: "18 de Julio",
+                respuesta: "3",
                 correcto: false
             },
             {
                 id: 2,
-                respuesta: "15 de Abril",
+                respuesta: "10",
                 correcto: false
             },
             {
                 id: 3,
-                respuesta: "20 de Julio",
-                correcto: true
+                respuesta: "5",
+                correcto: false
             },
             {
                 id: 4,
-                respuesta: "15 de Septiembre",
-                correcto: false
+                respuesta: "6",
+                correcto: true
             }
         ]
     },
     {
         id: 4,
-        encabezado: "Estas tres ciudades se localizan en las costas del mar Caribe:",
+        encabezado: "¿A qué ciudad se le conoce como “La ciudad de la eterna primavera”? ",
         opciones: [
             {
                 id: 1,
-                respuesta: "Tumacoí, Buenaventura y Nuquí.",
-                correcto: false
+                respuesta: "Medellin",
+                correcto: true
             },
             {
                 id: 2,
-                respuesta: "San Marta, Buenaventura, Pasto. ",
+                respuesta: "Bogotá",
                 correcto: false
             },
             {
                 id: 3,
-                respuesta: "Santa Marta, Barranquilla y Cartagena.",
+                respuesta: "Cartagena ",
+                correcto: false
+            },
+            {
+                id: 4,
+                respuesta: "Barranquilla",
+                correcto: false
+            }
+        ]
+    },
+    {
+        id: 5,
+        encabezado: "¿El río Amazonas y el Orinoco son las fronteras naturales de Colombia con?",
+        opciones: [
+            {
+                id: 1,
+                respuesta: "Brasil y Guyana",
+                correcto: false
+            },
+            {
+                id: 2,
+                respuesta: "Perú y Brasil",
+                correcto: false
+            },
+            {
+                id: 3,
+                respuesta: "Venezuela y Argentina",
+                correcto: false
+            },
+            {
+                id: 4,
+                respuesta: "Perú y Venezuela",
+                correcto: true
+            }
+        ]
+    },
+    {
+        id: 6,
+        encabezado: "¿La ausencia de estaciones en Colombia sedebe a?",
+        opciones: [
+            {
+                id: 1,
+                respuesta: "Que tiene montañas",
+                correcto: false
+            },
+            {
+                id: 2,
+                respuesta: "Que está en el trópico",
+                correcto: true
+            },
+            {
+                id: 3,
+                respuesta: "Los vientos alisios",
+                correcto: false
+            },
+            {
+                id: 4,
+                respuesta: "El fenómeno del Niño",
+                correcto: false
+            }
+        ]
+    },
+    {
+        id: 7,
+        encabezado: "¿Colombia es uno de los países del planetamás rico en?",
+        opciones: [
+            {
+                id: 1,
+                respuesta: "Razas de ganado",
+                correcto: false
+            },
+            {
+                id: 2,
+                respuesta: "Deciertos",
+                correcto: false
+            },
+            {
+                id: 3,
+                respuesta: "Biodiversidad",
                 correcto: true
             },
             {
                 id: 4,
-                respuesta: "Cali, Medellín y Pasto.",
+                respuesta: "Bosque primario",
+                correcto: false
+            }
+        ]
+    },
+    {
+        id: 8,
+        encabezado: "¿Cómo se llama y dónde está ubicada esta impresionante roca?",
+        opciones: [
+            {
+                id: 1,
+                respuesta: "Piedra del Tunjo, Facatativá, Departamento de Antioquia",
+                correcto: false
+            },
+            {
+                id: 2,
+                respuesta: "Piedra del Tunjo, Facatativá, Departamento de Cundinamarca",
+                correcto: false
+            },
+            {
+                id: 3,
+                respuesta: "Piedra del Peñol, en Guatapé, Departamento de Antioquia",
+                correcto: true
+            },
+            {
+                id: 4,
+                respuesta: "El tabor, en San Carlos, Departamento de Antioquia",
+                correcto: false
+            }
+        ]
+    },
+    {
+        id: 9,
+        encabezado: "Cali, la 3ª ciudad más grande de Colombia, ¿es considerada la capital mundial de?",
+        opciones: [
+            {
+                id: 1,
+                respuesta: "La corrida",
+                correcto: false
+            },
+            {
+                id: 2,
+                respuesta: "La caña de azucar",
+                correcto: false
+            },
+            {
+                id: 3,
+                respuesta: "La salsa",
+                correcto: true
+            },
+            {
+                id: 4,
+                respuesta: "La cumbia ",
                 correcto: false
             }
         ]
@@ -113,22 +245,41 @@ function inicializar() {
 }
 
 function pintar_preguntas(id_pregunta) {
-    const encabezado = document.getElementById('encabezado')
-    const opcion1 = document.getElementById('opcion1')
-    const opcion2 = document.getElementById('opcion2')
-    const opcion3 = document.getElementById('opcion3')
-    const opcion4 = document.getElementById('opcion4')
     let pregunta = preguntas.filter((pregunta) => { return pregunta.id === id_pregunta })
     pregunta = pregunta[0]
-    console.log(pregunta)
-    encabezado.innerHTML = pregunta.encabezado
-    opcion1.innerHTML = traer_respuesta(1, pregunta)
-    opcion2.innerHTML = traer_respuesta(2, pregunta)
-    opcion3.innerHTML = traer_respuesta(3, pregunta)
-    opcion4.innerHTML = traer_respuesta(4, pregunta)
+    /*condicional*/
+    if (pregunta) {
+        const encabezado = document.getElementById('encabezado')
+        const opcion1 = document.getElementById('opcion1')
+        const opcion2 = document.getElementById('opcion2')
+        const opcion3 = document.getElementById('opcion3')
+        const opcion4 = document.getElementById('opcion4')
+        encabezado.innerHTML = pregunta.encabezado
+        opcion1.innerHTML = traer_respuesta(1, pregunta)
+        opcion2.innerHTML = traer_respuesta(2, pregunta)
+        opcion3.innerHTML = traer_respuesta(3, pregunta)
+        opcion4.innerHTML = traer_respuesta(4, pregunta)
+    } else {
+        calcularPuntaje()
+    }
+}
+
+/*calcular el porcentaj*/
+async function calcularPuntaje() {
+    let puntosTotales = 0
+    await resultados.map(result => {
+        if (result) {
+            puntosTotales = puntosTotales + PUNTOS_POSITIVOS
+        } else {
+            puntosTotales = puntosTotales - PUNTOS_NEGATIVOS
+        }
+    })
+    if (puntosTotales < 0) puntosTotales = 0
+    alert("tu puntajes es" + puntosTotales + "de 1000" + " Deseas volver a intentarlo?")
 
 }
 
+/*filtro*/
 function traer_respuesta(id_respuesta, pregunta) {
     return pregunta.opciones.filter((opcion) => { return opcion.id === id_respuesta })[0].respuesta
 }
@@ -142,11 +293,11 @@ function validar_respuesta(id_respuesta) {
     pregunta = pregunta[0]
     const opciones = document.getElementsByClassName("opcion")
     if (respuesta_correcta(id_respuesta, pregunta)) {
-       resultados.push(true)
-    } else  {
-       resultados.push(false)
+        resultados.push(true)
+    } else {
+        resultados.push(false)
     }
-    console.log (resultados)
+    console.log(resultados)
     Array.from(opciones).forEach(element => {
         const opcion = "opcion" + id_respuesta
         if (opcion === element.id && respuesta_correcta(id_respuesta, pregunta)) {
@@ -156,35 +307,22 @@ function validar_respuesta(id_respuesta) {
         }
 
     });
-    
-
-
-    /*
-    if (respuesta_correcta(id_respuesta, pregunta)) {
-        opcion.classList.add("respuesta_correcta")
-    } else  {
-        opcion.classList.add("respuesta_incorrecta")
-    }
-    */
-
 }
 
+/*incremente la pregunta*/
 function siguiente_pregunta() {
     contador_preguntas = contador_preguntas + 1
     console.log(contador_preguntas)
-     document.getElementById('opcion1').classList.remove("respuesta_incorrecta");
-     document.getElementById('opcion2').classList.remove("respuesta_incorrecta");
-     document.getElementById('opcion3').classList.remove("respuesta_incorrecta");
-     document.getElementById('opcion4').classList.remove("respuesta_incorrecta");
-     document.getElementById('opcion1').classList.remove("respuesta_correcta");
-     document.getElementById('opcion2').classList.remove("respuesta_correcta");
-     document.getElementById('opcion3').classList.remove("respuesta_correcta");
-     document.getElementById('opcion4').classList.remove("respuesta_correcta");
+    /*remueve la seleccion de la pregunta anterior*/
+    document.getElementById('opcion1').classList.remove("respuesta_incorrecta");
+    document.getElementById('opcion2').classList.remove("respuesta_incorrecta");
+    document.getElementById('opcion3').classList.remove("respuesta_incorrecta");
+    document.getElementById('opcion4').classList.remove("respuesta_incorrecta");
+    document.getElementById('opcion1').classList.remove("respuesta_correcta");
+    document.getElementById('opcion2').classList.remove("respuesta_correcta");
+    document.getElementById('opcion3').classList.remove("respuesta_correcta");
+    document.getElementById('opcion4').classList.remove("respuesta_correcta");
     pintar_preguntas(contador_preguntas)
-
-
 }
-
-
 inicializar()
 
